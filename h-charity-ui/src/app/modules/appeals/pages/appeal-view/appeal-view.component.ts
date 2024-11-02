8774f552ad13e2fc3dd9ddbe6e87179149ae37de
developer-1-a <hcharitydev1@gmail.com>
2024-11-02 21:17:53 +0530

    Created about us page, added filters to appeals table and appeals and feedback buttons

--- h-charity-ui/src/app/modules/appeals/pages/appeal-view/appeal-view.component.ts
+++ h-charity-ui/src/app/modules/appeals/pages/appeal-view/appeal-view.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IAppeal } from '../../models/appeal.model';
 
 @Component({
   selector: 'app-appeal-view',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appeal-view.component.css']
 })
 export class AppealViewComponent implements OnInit {
+  @Input() appeal: any;
+  @Input() viewDialog: any;
+
+  submitted: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  viewAppeal(appeal: IAppeal) {
+    this.viewDialog = true;
+    this.appeal = appeal;
+  }
+
+  hideDialog() {
+    this.viewDialog = false;
+    this.submitted = false;
+  }
 }
